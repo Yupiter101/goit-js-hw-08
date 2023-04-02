371a207268b7bc1846f269d0cb9e43908636a0d4
Sergey_Sanyuk <shv08@ukr.net>
2023-04-02 21:50:16 +0300

    V3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,49 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+// console.log(galleryItems);
+
+const galaryRef = document.querySelector(".gallery");
+
+// === создание коллекции =====
+function createGalary(galary) {
+    const newGalleryItems = galary.map( ({preview, original, description}) => {
+    // li
+    const galaryItem = document.createElement('li');
+    galaryItem.classList.add("gallery__item");
+    // a
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add("gallery__link");
+    galleryLink.setAttribute('href', original);
+    // img 
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add("gallery__image");
+    galleryImage.src = preview;
+    // galleryImage.setAttribute('data-source', original);
+    // console.log(galleryImage);
+    galleryImage.alt = description;
+
+    galleryLink.append(galleryImage);
+    galaryItem.append(galleryLink);
+    // console.log(galaryItem);
+    // galaryRef.append(galaryItem);
+    // newGalleryItems.push(galaryItem);
+    return galaryItem;
+  });
+  galaryRef.append(...newGalleryItems);
+}
+
+createGalary(galleryItems);
+// === создание коллекции /конец=====
+
+const options = {
+    captionsData: "alt",
+    captionDelay: 250,
+};
+var lightbox = new SimpleLightbox('.gallery a', options);
